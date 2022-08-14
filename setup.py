cb1a923301f06f217434c2d8a6025eff6d90e7e9
yama55 <recmanpc@gmail.com>
2022-08-14 17:00:55 +0900

    romcan

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os, json, imp
 
 here = os.path.abspath(os.path.dirname(__file__))
 proj_info = json.loads(open(os.path.join(here, PROJ_METADATA)).read())
-README = open(os.path.join(here, 'README.rst')).read()
+README = open(os.path.join(here, 'README.rst'), encoding="utf-8").read()
 CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst')).read()
 VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
 
